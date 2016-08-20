b82ce213ad0b5538a5c498439897acf00f56f058
Nick Veys <nick@codelever.com>
2016-08-20 16:46:30 -0500

    Setting erlinit to use USB TTY

--- config/config.exs
+++ config/config.exs
@@ -8,6 +8,9 @@ use Mix.Config
 config :nerves_blinky, led_list: [ :red, :green ]
 config :nerves_leds, names: [ red: "led0", green: "led1" ]
 
+config :nerves, :firmware,
+  rootfs_additions: "config/rootfs-additions"
+
 # Import target specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 # Uncomment to use target specific configurations
